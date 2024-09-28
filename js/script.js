3b462467aa841d1f9c86f771d9139170b50ab767
Ahnaf Farhan Hossain <161995795+AhnafFarhanHossain@users.noreply.github.com>
2024-09-28 09:44:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay").style.top = "0";
+    document.getElementById("overlay").style.transition = "all .3s ease";
+}
+function closeNav() {
+    document.getElementById("overlay").style.display = "none";
+}
